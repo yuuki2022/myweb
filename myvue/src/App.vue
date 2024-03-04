cea4a9a565ce83999034768da09c67ce760bb44c
RTLiang <895302591@qq.com>
2024-03-04 18:20:47 +0800

    bug resolved

--- myvue/src/App.vue
+++ myvue/src/App.vue
@@ -2,23 +2,24 @@
   <router-link to="/login">Login</router-link>
   <router-link to="/student">Student</router-link>
   <router-view></router-view>
+  <login></login>
 </template>
 
 <script>
 
 // import router from './router';
-import login from './components/GradeLookup.vue'
+import login from './components/login.vue'
 
 export default {
   name: 'App',
   components: {
-   
+    login
   }
 }
 </script>
 
 <style>
-body{
+body {
   /*取消body标签的默认样式*/
   margin: 0;
   padding: 0;
@@ -26,14 +27,15 @@ body{
   width: 100vh;
   height: 80vh;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin:auto;
-  width:100%;
+  margin: auto;
+  width: 100%;
   height: 100%;
 }
 </style>
