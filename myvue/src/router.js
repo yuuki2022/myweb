a04a0cf88550a9d94915abec8e96735439f202b6
RTLiang <895302591@qq.com>
2024-03-06 15:04:48 +0800

    跑起来了

--- myvue/src/router.js
+++ myvue/src/router.js
@@ -4,41 +4,46 @@ import GradeLookup from './components/GradeLookup.vue';
 import Student from './components/Student.vue';
 import Register from './components/Register.vue';
 import CreateQuestion from './components/CreateQuestion.vue';
+import Exam from './components/Exam.vue'
 
 
 
 const routes = [
-    {
-      path: '/gradelookup',
-      name: 'gradelookup',
-      component: GradeLookup
-    },
-    {
-      path: '/',
-      name: 'login',
-      component: login
-    },
-    {
-      path: '/student',
-      name: 'student',
-      component: Student
-    },
-    {
-      path:"/register",
-      name:"register",
-      component: Register
-    },
-    {
-      path: '/createQuestion',
-      name:'CreateQuestion',
-      component: CreateQuestion
-    }
-  ];
-  
-  const router = createRouter({
-    history: createWebHistory(),
-    routes
-  });
-  
-  export default router;
-  
+  {
+    path: '/gradelookup',
+    name: 'gradelookup',
+    component: GradeLookup
+  },
+  {
+    path: '/',
+    name: 'login',
+    component: login
+  },
+  {
+    path: '/student',
+    name: 'student',
+    component: Student
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: '/createQuestion',
+    name: 'CreateQuestion',
+    component: CreateQuestion
+  }, 
+  {
+    path: '/exam',
+    name: 'Exam',
+    component: Exam
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
